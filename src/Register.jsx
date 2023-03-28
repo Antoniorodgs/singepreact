26293622245a277aa440301f27d729b24bed4b82
Gabriel Ganji <gabrielganji01011010@gmail.com>
2023-03-28 13:33:29 -0300

    adicionando campos Register.jsx

--- src/Register.jsx
+++ src/Register.jsx
@@ -4,6 +4,7 @@ export const Register = (props) => {
   const [email, setEmail] = useState('');
   const [pass, setPass] = useState('');
   const [name, setName] = useState('');
+  const [whatsapp, setWhatsapp] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -15,12 +16,21 @@ export const Register = (props) => {
       <h2>Registre-se</h2>
     <form className="register-form" onSubmit={handleSubmit}>
       <label htmlFor="name">Nome Completo</label>
-      <input value={name} name="name" id="name" placeholder="Nome"/>
+      <input name="name" id="name" placeholder="Nome"/>
       <label htmlFor="email">E-mail</label>
-      <input value={email} type="email" placeholder="Digite o seu e-mail" id="email" name="email"/>
+      <input type="email" placeholder="Digite o seu e-mail" id="email" name="email"/>
+
+      <label htmlFor="whatsapp">Whatsapp</label>
+      <input name="whatsapp" placeholder="1191234-5678" id="whatsapp"/>
+
+      <label htmlFor="establishment">Nome do estabelecimento</label>
+      <input type="name" placeholder="Nome fantásia" name="establishment"/>
 
       <label htmlFor="password">Senha</label>
-      <input value={pass} type="password" placeholder="********" id="password" name="password"/>
+      <input type="password" placeholder="********" id="password" name="password"/>
+      
+      <label htmlFor="passwordConfirm">Confirme a senha</label>
+      <input type="password" placeholder="********" id="password" name="passwordConfirm"/>
 
       <button type="submit">Conecte-se</button>
     </form>
