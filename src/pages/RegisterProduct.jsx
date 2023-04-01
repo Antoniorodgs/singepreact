51065746c475fcfca70f86b553d3262610eab18c
Antonio Rodrigues <antoniocarlosrodrigues2001@gmail.com>
2023-04-01 09:21:10 -0300

    tela registrar produto

--- src/pages/RegisterProduct.jsx
+++ src/pages/RegisterProduct.jsx
@@ -2,9 +2,53 @@ import React from 'react';
 
 export const RegisterProduct = () => {
     return (
-        <div>
-            <h3>Registrar</h3>
-            <p>Você está na página de registro de produto.</p>
+
+        <div className='registrarproduto'>
+        <section className='containerregistrarproduto'>
+        <header>Registrar Produto</header>
+        <form action="#" className="formreg"><br></br>
+            <div className='input-boxreg'>
+                <label htmlFor="">Nome</label>
+                <input type="text" placeholder='Nome do Produto'/>
+
+            </div>
+
+            <div className='input-boxreg'>
+                <label htmlFor="">Preço</label>
+                <input type="text" placeholder='R$'/>
+                
+            </div>
+
+            <div className='input-boxreg'>
+                <label htmlFor="">Lote</label>
+                <input type="text" placeholder='Lote do Produto'/>
+                
+            </div>
+
+            <div className='input-boxreg'>
+                <label htmlFor="">Data de validade</label>
+                <input type="date" placeholder='Data da Validade'/>
+                
+            </div>
+
+            <div className='input-boxreg'>
+                <label htmlFor="">Total de unidade</label>
+                <input type="text" placeholder='Total Unitario'/>
+                
+            </div>
+
+            <div className='input-boxreg'>
+                <label htmlFor="">Total em KG ou Quilogramas</label>
+                <input type="text" placeholder='Total em KG/G'/>
+                
+            </div>
+        </form>
+
+        <div className='columbtn'>
+        <button className='btncancel'>Cancelar</button>
+        <button className='btnreg'>Registrar</button>
         </div>
+        </section>
+    </div>
       );
 };
