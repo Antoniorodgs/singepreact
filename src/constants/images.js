9c510d038c980f2e9ba790bdb71aff31aacabbd8
Antonio Rodrigues <antoniocarlosrodrigues2001@gmail.com>
2023-04-02 11:49:57 -0300

    tela home

--- src/constants/images.js
+++ src/constants/images.js
@@ -4,9 +4,9 @@ const images = {
     singepLogo: require("../assets/images/logoSingep.png"),
     introducao: require("../assets/images/introducao.png"),
     avatar: require("../assets/images/testebitmoji.png"),
-    instagram: require("../assets/images/instagram.svg").default,
-    facebook: require("../assets/images/facebook.svg").default,
-    youtube: require("../assets/images/youtube.svg").default,
+    instagram: require("../assets/images/instagram-p.svg").default,
+    facebook: require("../assets/images/facebook-p.svg").default,
+    youtube: require("../assets/images/youtube-p.svg").default,
 }
 
 export default images
