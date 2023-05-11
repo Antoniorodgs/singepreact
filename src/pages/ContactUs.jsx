b280f3fc7778ad1344a2f5ff65d5aaa8cc8973fe
Antonio Rodrigues <antoniocarlosrodrigues2001@gmail.com>
2023-05-11 20:05:14 -0300

    tela contato

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -3,38 +3,38 @@ import React from 'react';
 export const ContactUs = () => {
     return (
         <div className='contato'>
+          <h3>Fale Conosco</h3><br /><hr />
             <section className='containercontato'>
-                <header> Fale Conosco </header>
+                
+                
                 <form action="#" className="formatt"><br></br>
             <div className='input-boxatt'>
                 <label htmlFor="">Assunto</label>
                 <div className='columnassunto'>
                     <div className='selectboxassunto'> 
                       <select>
-                        <option>Assunto 1</option>
-                        <option>Assunto 2</option>
-                        <option>Assunto 3</option>
-                        <option>Assunto 4</option>
-                        <option>Assunto 5</option>
+                        <option>Conta</option>
+                        <option>Ajuda</option>
+                        <option>Senha</option>
+                        <option>Sistema</option>
+                        <option>Notificações</option>
                         <option>Outros</option>
                       </select>
                     </div>
+
+
                 </div>
-
-            </div>
-
+            </div><br></br>
             <div className='areacontato'>
                 <label htmlFor="">Mensagem</label>
-                <input className='areacontato' type="text" placeholder='Conte-nos um pouco mais sobre'/>
-
+                <textarea className='' type="text" placeholder='Conte-nos um pouco mais sobre'/>
+                
             </div>
-
+            {/* <TextAreaExampleTextArea /> */}
             </form>
-
             <div className='columbtn'>
             <button className='btnreg'>Enviar</button>
             </div>
-
             </section>
         </div>
       );
