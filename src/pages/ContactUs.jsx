ed37ad7b0650ade830b6d818d5b5941ac578d40e
Antonio Rodrigues <antoniocarlosrodrigues2001@gmail.com>
2023-05-11 19:53:56 -0300

    contato atualizado

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,64 +1,39 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-
-const { io } = require("socket.io-client");
-
-let socket = io("http://localhost:3033");
 
 export const ContactUs = () => {
-
-    const allMsg = [];
-
-    let [message, setMessage] = useState('');
-    let [textArea, setTextArea] = useState('');
-    let [messagesArray, setMessagesArray] = useState([]);
-    
-    socket.on("msg", (data) => {
-      let newMsg = `${data.con.id} : ${data.con.msg}`;
-      setMessage(newMsg);
-      allMsg.push(newMsg);
-      setMessagesArray(allMsg)
-    })
-
-    const handleTextArea = async (e) => {
-      setTextArea(e.target.value);
-    }
-
-    const handleSubmit = async (e) => {
-
-        e.preventDefault();
-        socket.emit("message", {msg: textArea, id: socket.id});
-
-    }
-
     return (
         <div className='contato'>
-
-            <div id="contactArea">
-              <div id="usCompany">
-                <h4>SINGEP</h4>
-              </div>
-              <h4>Message: </h4><br></br>
-              <p>
-              {
-                message
-              }</p>
+            <section className='containercontato'>
+                <header> Fale Conosco </header>
+                <form action="#" className="formatt"><br></br>
+            <div className='input-boxatt'>
+                <label htmlFor="">Assunto</label>
+                <div className='columnassunto'>
+                    <div className='selectboxassunto'> 
+                      <select>
+                        <option>Assunto 1</option>
+                        <option>Assunto 2</option>
+                        <option>Assunto 3</option>
+                        <option>Assunto 4</option>
+                        <option>Assunto 5</option>
+                        <option>Outros</option>
+                      </select>
+                    </div>
+                </div>
 
             </div>
-            
-            <section className='containercontato'>
-                <form action="#" className="formatt" id="contactUs" onSubmit={handleSubmit}>
-                  <div id="fieldButton">
-                    <textarea onChange={handleTextArea} className='textArea' type="text" placeholder='Conte-nos um pouco mais sobre'/>
-                    <button type="submit" form="contactUs" className='btnreg' id='sendMessage'>Enviar</button>
-                  </div>
-                </form>
 
-                <div className='columbtn'>
+            <div className='areacontato'>
+                <label htmlFor="">Mensagem</label>
+                <input className='areacontato' type="text" placeholder='Conte-nos um pouco mais sobre'/>
 
-                </div>
+            </div>
+
+            </form>
+
+            <div className='columbtn'>
+            <button className='btnreg'>Enviar</button>
+            </div>
 
             </section>
         </div>
